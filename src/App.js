b5e39907892130c13cdeed2f32bcae64c0229258
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-02-27 14:07:17 +0100

    removed unnecessary files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project was coded by Elisabeth Erkekoglou {""}
+        <a
+          href="https://github.com/elic4vet/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          and it's open-sourced on github
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
