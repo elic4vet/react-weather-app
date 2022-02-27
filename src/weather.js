ca935b7a816e20b1255a5d05b45e4c980db4a0f0
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-02-27 20:32:12 +0100

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -3,17 +3,22 @@ import "./weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
+    <div className="weather">
       <form>
         <div className="row">
           <input
             type="search"
             placeholder="Enter a city.."
             className="form-control"
+            autoFocus="on"
           />
 
           <div className="col-6">
-            <input type="submit" value="search" className="btn-btn-primary" />
+            <input
+              type="submit"
+              value="search"
+              className="btn-btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -24,19 +29,24 @@ export default function Weather() {
         <li>Mostly cloudy </li>
       </ul>
 
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://img.icons8.com/dusk/64/000000/partly-cloudy-day--v2.png"
-            alt="mostly-cloudy"
-          />
-          <div className="col-6">
-            <ul>
-              <li>Precipitation 15%</li>
-              <li>Humidity % </li>
-              <li> Wind : 13km/h% </li>
-            </ul>
+          <div className="clearfix">
+            <img
+              src="https://img.icons8.com/dusk/64/000000/partly-cloudy-day--v2.png"
+              alt="mostly-cloudy"
+            />
+            <span className="temperature">6</span>°C
+            <span className="unit"> </span>
           </div>
+        </div>
+
+        <div className="col-6">
+          <ul>
+            <li>Precipitation 15%</li>
+            <li>Humidity % </li>
+            <li> Wind : 13km/h% </li>
+          </ul>
         </div>
       </div>
     </div>
