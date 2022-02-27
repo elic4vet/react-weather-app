05d37860dba1c9a49806743326ebbbe22d3bd4dd
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-02-27 15:25:06 +0100

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -4,6 +4,20 @@ import "./weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            className="form-control"
+          />
+
+          <div className="col-6">
+            <input type="submit" value="search" className="btn-btn-primary" />
+          </div>
+        </div>
+      </form>
+
       <h1> New York </h1>
       <ul>
         <li>Wednesday 07:00</li>
@@ -12,12 +26,10 @@ export default function Weather() {
 
       <div className="row">
         <div className="col-6">
-          <img>
-            <img
-              src="https://img.icons8.com/dusk/64/000000/partly-cloudy-day--v2.png"
-              alt="mostly cloudy"
-            />
-          </img>
+          <img
+            src="https://img.icons8.com/dusk/64/000000/partly-cloudy-day--v2.png"
+            alt="mostly-cloudy"
+          />
           <div className="col-6">
             <ul>
               <li>Precipitation 15%</li>
