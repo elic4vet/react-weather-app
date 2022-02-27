0a23cc83f48055bc6200c2a24786fa3a737b861a
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-02-27 14:43:53 +0100

    removed some unsed files / changed some files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      Weather app
+      <footer>
+        This project was coded by Elisabeth Erkekoglou {""}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href=" https://github.com/elic4vet/react-weather-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          and it is open sourced
+        </a>{" "}
+      </footer>
     </div>
   );
 }
