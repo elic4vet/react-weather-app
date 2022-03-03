7626fca0917e30bbfcb302b396add3f1e8330001
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-03-03 20:45:55 +0100

    Update weatherInfo.js

--- src/weatherInfo.js
+++ src/weatherInfo.js
@@ -1,5 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,12 +15,12 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img src={props.data.icon} alt={props.data.decription} />
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            °C
-            <span className="unit"> </span>
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
           </div>
         </div>
 
