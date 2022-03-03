53e62cc32b8206f3808e3c8c5daf61daf5960ed4
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-03-03 20:57:14 +0100

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "c71a6a7a6861fb5c16cd1f49a0f5bc4f";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
