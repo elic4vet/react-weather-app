440ad1f320c0422dfe36242bb3a1f4b0f249b63f
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-03-04 20:34:35 +0100

    Update weatherInfo.js

--- src/weatherInfo.js
+++ src/weatherInfo.js
@@ -8,34 +8,27 @@ export default function WeatherInfo(props) {
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
       <ul>
-        <FormattedDate date={props.data.date} />
-        <li className="text-capitalize">{props.data.description} </li>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
       </ul>
-
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
+
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
-
         <div className="col-6">
           <ul>
-            <li>Precipitation 15%</li>
-            <li>
-              Humidity {""}
-              {props.data.humidity}%{" "}
-            </li>
-            <li>
-              {" "}
-              Wind {""}
-              {props.data.wind} km/h
-            </li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
