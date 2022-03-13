b6a94cc18f1b19e3fc13f4cdcb2518e5977490d4
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-03-13 18:44:42 +0100

    Update weatherForecastDay.js

--- src/weatherForecastDay.js
+++ src/weatherForecastDay.js
@@ -21,16 +21,18 @@ export default function WeatherForecastDay(props) {
   }
   return (
     <div>
-      <div className="WeatherForecast-day">{day()} </div>
-      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      <div className="WeatherForecast-day">
+        {day()}
+        <WeatherIcon code={props.data.weather[0].icon} size={36} />
 
-      <div className="WeatherForecast-temperatures">
-        <span className="WeatherForecast-temperature-max">
-          {maxTemperature()}
-        </span>
-        <span className="WeatherForecast-temperature-min">
-          {minTemperature()}
-        </span>
+        <div className="WeatherForecast-temperatures">
+          <span className="WeatherForecast-temperature-max">
+            {maxTemperature()}
+          </span>
+          <span className="WeatherForecast-temperature-min">
+            {minTemperature()}
+          </span>
+        </div>
       </div>
     </div>
   );
